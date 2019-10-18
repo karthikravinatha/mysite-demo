650eebf9cf192eae02c481650f713f6cec0f7c8c
kar <kar@gmail.com>
2019-10-18 10:32:32 +0530

    18.10 commit

--- polls/admin.py
+++ polls/admin.py
@@ -8,8 +8,8 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     # list_display = ('question_text','pub_date','was_published_recently')
     # fieldsets = [
-    #     (None, {'fields': ['question_text']}),
-    #     ('Date Information', {'fields': ['pub_date'],'classes':['collapse']}),
+    #     (None, {'fiel ds': ['question_text']}),
+    #     ('Date Infor mation', {'fields': ['pub_date'],'classes':['collapse']}),
     # ]
     inlines = [ChoiceInline]
 
